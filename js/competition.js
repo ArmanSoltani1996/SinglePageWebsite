28547fe76254f75fa16475ede3c9dd7cc467a25d
Arman Soltani <51240250+ArmanSoltani1996@users.noreply.github.com>
2026-09-18 00:54:16 +0330

    Update competition.js

--- js/competition.js
+++ js/competition.js
@@ -50,6 +50,7 @@ const FIXTURES = [
   
   { group: "گروه ۳", home: "پتروشیمی شیراز", away: "فولاد غدیر نی ریز", status: "played", score: "2 - 3", date: "۱۴۰۵/۰۶/۲۴", time: "۱۴:۱۵" }, //
   { group: "گروه ۲", home: "پارس الکل اقلید", away: "زنجیره سالیذ", status: "played", score: "0 - 4", date: "۱۴۰۵/۰۶/۲۴", time: "۱۵:۱۵" },//
+   
   { group: "گروه ۱", home: "آرتا تجارت", away: "نظام مهندسی", status: "played", score: "2 - 1", date: "۱۴۰۵/۰۶/۲۵", time: "۱۱:۰۰" },//
   { group: "گروه ۴", home: "شام شام", away: "فراسان", status: "played", score: "8 - 1", date: "۱۴۰۵/۰۶/۲۵", time: "۱۲:۰۰" },//
   { group: "گروه ۲", home: "پگاه فارس", away: "پارس الکل اقلید", status: "played", score: "4 - 1", date: "۱۴۰۵/۰۶/۲۵", time: "۱۳:۰۰" },//
@@ -63,9 +64,10 @@ const FIXTURES = [
   { group: "گروه ۴", home: "شهرداری شیراز", away: "فراسان", status: "upcoming", date: "1405/07/01", time: "13" },
   { group: "گروه ۳", home: "یاسین پلاست", away: "فولاد غدیر نی ریز", status: "upcoming", date: "1405/07/01", time: "14" },
    
-  { group: "گروه ۱", home: "آرتا تجارت", away: "صنایع شیمیایی فارس", status: "upcoming", date: "**", time: "**" },
-  { group: "گروه ۲", home: "رامک", away: "زنجیره سالیذ", status: "upcoming", date: "**", time: "**" },
-  { group: "گروه ۳", home: "پتروشیمی شیراز", away: "گاز استان", status: "upcoming", date: "**", time: "**" },
+  { group: "گروه ۱", home: "آرتا تجارت", away: "صنایع شیمیایی فارس", status: "upcoming", date: "1405/07/04", time: "12:30" },
+  { group: "گروه ۳", home: "پتروشیمی شیراز", away: "گاز استان", status: "upcoming", date: "1405/07/04", time: "13:30" }, 
+  { group: "گروه ۲", home: "رامک", away: "زنجیره سالیذ", status: "upcoming", date: "1405/07/04", time: "14:30" },
+  
   { group: "گروه ۲", home: "رامک", away: "پگاه فارس", status: "upcoming", date: "**", time: "**" },
   { group: "گروه ۳", home: "یاسین پلاست", away: "پتروشیمی شیراز", status: "upcoming", date: "**", time: "**" },
   { group: "گروه ۱", home: "سیمان فارس نو", away: "آرتا تجارت", status: "upcoming", date: "**", time: "**" },
