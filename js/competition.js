317f65b64817ade18c9a615ff4abc0850b1ef113
Arman Soltani <51240250+ArmanSoltani1996@users.noreply.github.com>
2026-09-23 16:24:30 +0330

    Update competition.js

--- js/competition.js
+++ js/competition.js
@@ -62,7 +62,7 @@ const FIXTURES = [
   { group: "گروه ۱", home: "سیمان فارس نو", away: "نظام مهندسی", status: "played", score: "3 - 3", date: "1405/07/01", time: "12:00" },
   { group: "گروه ۲", home: "رامک", away: "پارس الکل اقلید", status: "upcoming", date: "1405/07/01", time: "13:00" },
   { group: "گروه ۴", home: "شهرداری شیراز", away: "فراسان", status: "played", score: "11 - 2", date: "1405/07/01", time: "14:00" },
-  { group: "گروه ۳", home: "یاسین پلاست", away: "فولاد غدیر نی ریز", status: "upcoming", date: "1405/07/01", time: "15:00" },
+  { group: "گروه ۳", home: "یاسین پلاست", away: "فولاد غدیر نی ریز", status: "played", score: "0 - 0", date: "1405/07/01", time: "15:00" },
    
   { group: "گروه ۱", home: "آرتا تجارت", away: "صنایع شیمیایی فارس", status: "upcoming", date: "1405/07/04", time: "12:30" },
   { group: "گروه ۳", home: "پتروشیمی شیراز", away: "گاز استان", status: "upcoming", date: "1405/07/04", time: "13:30" }, 
